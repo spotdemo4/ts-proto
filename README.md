255844f0cab43809d4b1d4e00585ef12e30f9c5d
trev <me@trev.xyz>
2025-07-17 07:39:36 -0400

    feat: flake, renovate, readme, etc

---
+++ README.md
@@ -0,0 +1,10 @@
+[![GitHub](https://img.shields.io/badge/GitHub-%23121011.svg?logo=github&logoColor=white)](https://github.com/spotdemo4/ts-proto)
+[![Gitea](https://img.shields.io/badge/Gitea-6eaa5b?logo=gitea&logoColor=fff)](https://gitea.com/spotdemo4/ts-proto)
+[![Codeberg](https://img.shields.io/badge/Codeberg-2185D0?logo=codeberg&logoColor=fff)](https://codeberg.org/spotdemo4/ts-proto)
+
+# Trevstack Web
+
+[![check](https://img.shields.io/github/actions/workflow/status/spotdemo4/ts-proto/check.yaml?logo=GitHub&logoColor=%23cdd6f4&label=check&labelColor=%2311111b)](https://github.com/spotdemo4/ts-proto/actions/workflows/check.yaml)
+[![flake](https://img.shields.io/github/actions/workflow/status/spotdemo4/ts-proto/flake.yaml?logo=nixos&logoColor=%2389dceb&label=flake&labelColor=%2311111b)](https://github.com/spotdemo4/ts-proto/actions/workflows/flake.yaml)
+
+see [github.com/spotdemo4/trevstack](https://github.com/spotdemo4/trevstack)
---
+++ flake.nix
@@ -0,0 +1,123 @@
+{
+  description = "Trevstack Protobufs";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://trevnur.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "trevnur.cachix.org-1:hBd15IdszwT52aOxdKs5vNTbq36emvEeGqpb25Bkq6o="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    nur,
+    ...
+  }: let
+    build-systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forSystem = f:
+      nixpkgs.lib.genAttrs build-systems (
+        system:
+          f {
+            inherit system;
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [nur.overlays.default];
+            };
+          }
+      );
+  in rec {
+    devShells = forSystem ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          git
+          buf
+
+          # Nix
+          nix-update
+          alejandra
+
+          # Actions
+          prettier
+          action-validator
+          renovate
+        ];
+        shellHook = ''
+          echo "nix flake check --accept-flake-config" > .git/hooks/pre-commit
+          chmod +x .git/hooks/pre-commit
+        '';
+      };
+    });
+
+    checks = forSystem ({
+      system,
+      pkgs,
+      ...
+    }:
+      pkgs.nur.repos.trev.lib.mkChecks {
+        lint = {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            alejandra
+            prettier
+            action-validator
+            renovate
+          ];
+          checkPhase = ''
+            alejandra -c .
+            prettier --check .
+            action-validator .github/workflows/*
+            action-validator .gitea/workflows/*
+            action-validator .forgejo/workflows/*
+            renovate-config-validator
+          '';
+          installPhase = ''
+            cp -r .cache/buf "$out"
+          '';
+          outputHashAlgo = "sha256";
+          outputHashMode = "recursive";
+          outputHash = "sha256-W3141wtpQ4OHrEV+2soKzSiMsFiCVeSShbpOFUASe84=";
+        };
+      }
+      // {
+        buf = pkgs.stdenv.mkDerivation {
+          name = "buf-check";
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            buf
+          ];
+          checkPhase = ''
+            HOME=$(pwd)
+            buf lint
+          '';
+          buildPhase = ''
+            HOME=$(pwd)
+            buf dep graph
+          '';
+          installPhase = ''
+            cp -r .cache/buf "$out"
+          '';
+          outputHashAlgo = "sha256";
+          outputHashMode = "recursive";
+          outputHash = "sha256-W3141wtpQ4OHrEV+2soKzSiMsFiCVeSShbpOFUASe84=";
+        };
+
+        shell = devShells."${system}".default;
+      });
+
+    formatter = forSystem ({pkgs, ...}: pkgs.alejandra);
+  };
+}
